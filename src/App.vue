c8209d9167fd620c933041703115779659f88823
QiuTianJia <997348985@qq.com>
2020-03-27 01:05:08 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <HomePage />
+    </div>
+</template>
+
+<script>
+    import HomePage from "@/components/HomePage";
+
+
+    export default {
+        name: 'App',
+        components: {
+            HomePage
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <video-player :options="videoOptions"/>
+    </div>
+</template>
+
+<script>
+    import VideoPlayer from "@/components/VideoPlayer";
+
+    export default {
+        name: "HomePage",
+        components: {
+            VideoPlayer
+        },
+        data() {
+            return {
+                videoOptions: {
+                    autoplay: true,
+                    controls: true,
+                    sources: [
+                        {
+                            src: "https://video2.hsanhl.com/btt1/2020/03/20200325/oWvwdzmz/index.m3u8",
+                            type: "application/x-mpegURL"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
