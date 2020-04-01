806667c198329a6c721c52e4523639ce403e1b92
QiuTianJia <qiutianjia>
2020-04-01 15:40:29 +0800

    用户中心个人资料优化

--- src/components/My/Favorite.vue
+++ src/components/My/Favorite.vue
@@ -1,11 +1,89 @@
 <template>
-    <div>456</div>
+    <a-table
+            :columns="columns"
+            :rowKey="record => record.login.uuid"
+            :dataSource="data"
+            :pagination="pagination"
+            :loading="loading"
+            @change="handleTableChange"
+    >
+        <template slot="name" slot-scope="name">
+            {{name.first}} {{name.last}}
+        </template>
+    </a-table>
 </template>
-
 <script>
+    import reqwest from 'reqwest';
+    const columns = [
+        {
+            title: '收藏时间',
+            sorter: true,
+            dataIndex: 'gender',
+            width: '20%',
+        },
+        {
+            title: '.M3U8',
+            dataIndex: 'name',
+            sorter: true,
+            scopedSlots: { customRender: 'name' },
+        },
+        {
+            title: '操作',
+            dataIndex: 'email',
+            width: '20%',
+        },
+    ];
+
     export default {
-        name: "Favorite"
-    }
+        name: 'Favorite',
+        mounted() {
+            this.fetch();
+        },
+        data() {
+            return {
+                data: [],
+                pagination: {},
+                loading: false,
+                columns,
+            };
+        },
+        methods: {
+            handleTableChange(pagination, filters, sorter) {
+                console.log(pagination);
+                const pager = { ...this.pagination };
+                pager.current = pagination.current;
+                this.pagination = pager;
+                this.fetch({
+                    results: pagination.pageSize,
+                    page: pagination.current,
+                    sortField: sorter.field,
+                    sortOrder: sorter.order,
+                    ...filters,
+                });
+            },
+            fetch(params = {}) {
+                console.log('params:', params);
+                this.loading = true;
+                reqwest({
+                    url: 'https://randomuser.me/api',
+                    method: 'get',
+                    data: {
+                        results: 10,
+                        ...params,
+                    },
+                    type: 'json',
+                }).then(data => {
+                    const pagination = { ...this.pagination };
+                    // Read total count from server
+                    // pagination.total = data.totalCount;
+                    pagination.total = 200;
+                    this.loading = false;
+                    this.data = data.results;
+                    this.pagination = pagination;
+                });
+            },
+        },
+    };
 </script>
 
 <style scoped>
--- src/components/My/Info.vue
+++ src/components/My/Info.vue
@@ -1,19 +1,63 @@
 <template>
-    <a-upload
-            name="avatar"
-            listType="picture-card"
-            class="avatar-uploader"
-            :showUploadList="false"
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            :beforeUpload="beforeUpload"
-            @change="handleChange"
-    >
-        <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
-        <div v-else>
-            <a-icon :type="loading ? 'loading' : 'plus'" />
-            <div class="ant-upload-text">Upload</div>
-        </div>
-    </a-upload>
+    <div>
+        <a-row>
+            <a-col :span="6">
+                <a-upload
+                        name="avatar"
+                        listType="picture-card"
+                        class="avatar-uploader"
+                        :showUploadList="false"
+                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                        :beforeUpload="beforeUpload"
+                        @change="handleChange"
+                >
+                    <a-avatar v-if="imageUrl" :src="imageUrl" alt="avatar" :size="128"/>
+                    <div v-else>
+                        <a-icon :type="loading ? 'loading' : 'plus'"/>
+                        <div class="ant-upload-text">上传</div>
+                    </div>
+                </a-upload>
+                <a-form
+                        id="info"
+                        :form="form"
+                        class="info-form"
+                        @submit="handleSubmit"
+                >
+                    <a-form-item>
+                        <a-input
+                                v-decorator="[
+                        'nickname',
+                        {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: '请输入昵称',
+                                    whitespace: true
+                                }
+                            ],
+                        },
+                    ]"
+                                placeholder="昵称"
+                        >
+                            <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)"/>
+                        </a-input>
+                    </a-form-item>
+                    <a-button type="primary" html-type="submit" class="info-form-button">
+                        修改
+                    </a-button>
+                </a-form>
+            </a-col>
+            <a-col :span="4" offset="1">
+                <a-statistic title="区域" value="东莞" />
+            </a-col>
+            <a-col :span="4">
+                <a-statistic title="IP" value="127.0.0.1" />
+            </a-col>
+            <a-col :span="4">
+                <a-statistic title="设备" value="浏览器" />
+            </a-col>
+        </a-row>
+    </div>
 </template>
 <script>
     function getBase64(img, callback) {
@@ -21,15 +65,31 @@
         reader.addEventListener('load', () => callback(reader.result));
         reader.readAsDataURL(img);
     }
+
     export default {
         name: 'Info',
         data() {
             return {
                 loading: false,
-                imageUrl: '',
+                imageUrl: 'https://randomuser.me/api/portraits/women/68.jpg',
             };
         },
+        beforeCreate() {
+            this.form = this.$form.createForm(this, {name: 'info'});
+        },
         methods: {
+            handleSubmit(e) {
+                e.preventDefault();
+                this.form.validateFieldsAndScroll((err, values) => {
+                    if (!err) {
+                        console.log('Received values of form: ', values);
+                    }
+                });
+            },
+            handleConfirmBlur(e) {
+                const value = e.target.value;
+                this.confirmDirty = this.confirmDirty || !!value;
+            },
             handleChange(info) {
                 if (info.file.status === 'uploading') {
                     this.loading = true;
@@ -46,13 +106,13 @@
             beforeUpload(file) {
                 const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
                 if (!isJpgOrPng) {
-                    this.$message.error('You can only upload JPG file!');
+                    this.$message.error('只支持jpeg和png格式的图片');
                 }
-                const isLt2M = file.size / 1024 / 1024 < 2;
-                if (!isLt2M) {
-                    this.$message.error('Image must smaller than 2MB!');
+                const isLt50KB = file.size / 1024 < 50;
+                if (!isLt50KB) {
+                    this.$message.error('上传的图片必须小于50KB');
                 }
-                return isJpgOrPng && isLt2M;
+                return isJpgOrPng && isLt50KB;
             },
         },
     };
@@ -62,6 +122,7 @@
         width: 128px;
         height: 128px;
     }
+
     .ant-upload-select-picture-card i {
         font-size: 32px;
         color: #999;
@@ -70,5 +131,13 @@
     .ant-upload-select-picture-card .ant-upload-text {
         margin-top: 8px;
         color: #666;
+    }
+
+    #info {
+        margin-top: 20px;
+    }
+
+    #info .info-form-button {
+        width: 100%;
     }
 </style>
